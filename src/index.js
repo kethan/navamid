fb06750d20ea2e2c3a4904d893295dcf3cfb3ccf
Kethan Chandrakanth <kethanc@tissrv.com>
2022-06-30 06:29:18 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ export default function (base, on404, onErr = () => { }) {
     onError = onErr;
   $.req = { url: {}, params: {} };
   $.res = {
-    run: $.run,
-    redirect: $.route,
+    run: (uri) => $.run(uri),
+    redirect: (uri, replace) => $.route(uri, replace)
   };
   var fmt = ($.format = function (uri) {
     if (!uri) return uri;
